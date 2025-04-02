a8868a48d05da2cfbdf0a84dea69f0f059b77396
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-04-02 15:33:02 +0200

    fix: __dirname not available when using ESM

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from "electron";
 import path from "node:path";
+const __dirname = import.meta.dirname;
 
 const createWindow = () => {
   const win = new BrowserWindow({
